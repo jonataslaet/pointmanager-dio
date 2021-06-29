28bf6a11e0748fa34b34029382acef09fe3617a0
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-06-29 00:39:38 -0300

    Projeto criado

---
+++ src/main/java/com/digitalinnovation/one/pointmanagerdio/PointmanagerDioApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.one.pointmanagerdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointmanagerDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointmanagerDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalinnovation/one/pointmanagerdio/PointmanagerDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.one.pointmanagerdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointmanagerDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
